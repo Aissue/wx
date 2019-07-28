453125ed997750a4f36119e76c54006292b7ffed
wangkun <aissue@126.com>
2019-07-28 11:31:18 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+{
+  Page({
+  
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      name:"aissue",
+      age:18,
+      students:[
+        { id: 1, name: "kobe", age: 18 },
+        { id: 1, name: "aissue", age: 20 },
+        { id: 1, name: "sjj", age: 28 }
+      ],
+      counter: 0
+
+      
+    },
+    add(){
+      this.setData({
+        counter: this.data.counter + 1
+      })
+    },
+    sub() {
+      this.setData({
+        counter: this.data.counter - 1
+      })
+    }
+  
+    
+  })
+}
